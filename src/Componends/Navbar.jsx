b4fced1defa1f942a6b890af14ac5585259f3807
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-11-05 23:56:39 +0530

    first commit

--- src/Componends/Navbar.jsx
+++ src/Componends/Navbar.jsx
@@ -8,6 +8,20 @@ function Navbar() {
         <img className='ml-[1.3vw] h-[6vh]' src={logo} alt="" />
         <div className='bg-white text-black px-[2vw] py-[1vh] rounded-full text-[1.1vw]'>Menu</div>
       </div>
+      <div className='flex justify-end'>
+        <div className='hiddenmenu h-[60vh] w-[25vw] bg-red-500 mt-[2vh] rounded-[3vh] relative'> 
+          <div className=' absolute right-[1vw] '><i class="ri-close-circle-fill ri-3x"></i></div>
+          <div className=' absolute top-[10vh] left-[2vw] text-[1.8vw] font-light'>
+            <h1>Events</h1>
+            <h1>Core Members</h1>
+            <h1>Executive Members</h1>
+            <h1>Collaborate<i class="ri-add-line"></i></h1>
+          </div>
+          <div className='absolute top-[35vh] left-[2vw]'>
+            <h1 className='font-bold text-[1.8vw]'>Get in touch !</h1>
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
