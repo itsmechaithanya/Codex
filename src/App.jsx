be9107b1e699643855b3ec859b901f65afc571c7
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-11-08 09:07:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
+import Lenis from 'lenis'
 import React, { useEffect } from 'react'
 import Home from './Components/Home'
-import Lenis from 'lenis'
+import CoreMembers from './Components/CoreMembers'
+import ExecutiveMembers from './Components/ExecutiveMembers'
+import Collaborate from './Components/Collaborate'
+import Navbar from './Components/Navbar';
+import { Route, Routes } from 'react-router-dom'
+import Events from './Components/Events';
 
 
 function App() {
   useEffect(() => {
-    // Initialize Lenis
     const lenis = new Lenis();
-
-    // Listen for the scroll event and log the event data
-    // lenis.on('scroll', (e) => {
-    //   console.log(e);
-    // });
-
-    // Use requestAnimationFrame to continuously update the scroll
     function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
+    
+    // Cleanup function to cancel the animation frame
+    return () => {
+      cancelAnimationFrame(raf);
+    };
   }, []);
 
   return (
     <div>
-      <Home/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Events' element={<Events/>}/>
+        <Route path='/CoreMembers' element={<CoreMembers/>}/>
+        <Route path='/ExecutiveMembers' element={<ExecutiveMembers/>}/>
+        <Route path='/Collaborate' element={<Collaborate/>}/>
+      </Routes>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import git from '../assets/git.png'
 import link from '../assets/link.png'
@@ -17,7 +18,7 @@ function Navbar() {
   };
 
   return (
-    <div className='pl-[5vw] pt-[3vh] fixed z-30'>
+    <div className='pl-[5vw] pt-[3vh] fixed z-30 text-white'>
       <div className='w-[90vw] h-[8vh] rounded-full flex justify-between items-center px-[1vw] backdrop-blur-md bg-[#0000003b]  text-white border'>
         <img className='ml-[1.3vw] h-[6vh]' src={logo} alt="" />
         <button className='bg-white text-black px-[2vw] py-[1vh] rounded-full text-[1.1vw] open' onClick={openMenu}>Menu</button>
@@ -25,19 +26,19 @@ function Navbar() {
       <div className='flex justify-end -mr-[1vw]'>
         <div className={`hiddenmenu h-[60vh] w-[25vw] backdrop-blur-md bg-[#0000003b] mt-[2vh] rounded-[3vh] relative ${isMenuVisible ? 'block' : 'hidden'}`}> 
           <button className='absolute right-[1vw] close' onClick={closeMenu}><i class="ri-close-circle-fill ri-3x"></i></button>
-          <div className='absolute top-[10vh] left-[2vw] text-[1.8vw] font-light'>
-            <h1>Events</h1>
-            <h1>Core Members</h1>
-            <h1>Executive Members</h1>
-            <h1>Collaborate<i class="ri-add-line"></i></h1>
+          <div className='absolute top-[10vh] left-[2vw] text-[1.8vw] font-light '>
+            <Link to="/Events" onClick={closeMenu}><h1>Events</h1></Link>
+            <Link to="/CoreMembers" onClick={closeMenu}><h1>Core Members</h1></Link>
+            <Link to="/ExecutiveMembers" onClick={closeMenu}><h1>Executive Members</h1></Link>
+            <Link to="/Collaborate" onClick={closeMenu}><h1>Collaborate<i class="ri-add-line"></i></h1></Link>
           </div>
           <div className='absolute top-[35vh] left-[2vw]'>
             <h1 className='font-bold text-[1.8vw] mt-[4vh]'>Get in touch !</h1>
             <div className='flex justify-between w-[20vw] mt-[1vh]'>
-              <img className='h-[4vw]' src={git} alt="" />
-              <img className='h-[4vw]' src={link} alt="" />
-              <img className='h-[4vw]' src={x} alt="" />
-              <img className='h-[4vw]' src={ins} alt="" />
+              <Link to="https://github.com/CODEX-WoU/"><img className='h-[4vw]' src={git} alt="" /></Link>
+              <Link to="https://www.linkedin.com/company/codex-wou/"><img className='h-[4vw]' src={link} alt="" /></Link>
+              <Link to="https://x.com/CodeX_WOU"><img className='h-[4vw]' src={x} alt="" /></Link>
+              <Link to="https://www.instagram.com/codex_wou/"><img className='h-[4vw]' src={ins} alt="" /></Link>
             </div>
             <div className='flex justify-center mt-[2vh]'>
               <h1 className='text-[1.3vw]'>codex@woxsen.edu.in</h1>
