272117c821014689225873f9046618d66b22f102
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-11-08 20:16:04 +0530

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,8 @@ import footer from '../assets/footer.png'
 
 function Footer() {
   return (
-    <div className='w-screen h-[60vh] overflow-hidden relative'>
+    <div className='w-screen overflow-hidden'>
+      <div className='w-screen h-[60vh] overflow-hidden relative'>
       <div className=' absolute bottom-[-40vh] left-[-35vh]'><img className='h-[100vh]' src={footer} alt="" /></div>
       <div className='h-[50vh] w-screen flex justify-center items-center '><h1 className='text-[2vw]'>Code the future to create impact and craft innovation</h1></div>
       <div className='flex justify-center'>
@@ -12,6 +13,7 @@ function Footer() {
       <div className='h-[10vh] flex justify-center items-center'>
         <h1>© 2024 Copyright: CodeX - The Programming Club, Woxsen University</h1>
       </div>
+    </div>
     </div>
   )
 }
