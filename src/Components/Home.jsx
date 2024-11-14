db98f885dc591ef94d447f327298a32fae84f725
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-11-14 16:15:17 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
     <div className='bg-black text-white w-full overflow-hidden h-fit'>
         
       <div className='w-full h-screen relative'>
-        <div className='absolute z-10 lg:bottom-[10vh] bottom-[20vh] left-[5vw] lg:text-[8vw] text-[7vh] lg:leading-[6.5vw] leading-[7.5vh] font-extrabold'>Code. <br /> Create. <br /> Collaborate.</div>
+        <div className='absolute z-10 lg:bottom-[10vh] bottom-[20vh] left-[5vw] lg:text-[8vw] text-[7vh] lg:leading-[7.5vw] leading-[7.5vh] font-extrabold'>Code. <br /> Create. <br /> Collaborate.</div>
         <div><img className='lg:h-[100vw] h-[100vh] object-cover lg:bottom-[-70vh] bottom-[-10vh] absolute' src={hero} alt="" /></div>
         <div className=' absolute top-[20vh] right-[5vw] lg:text-[1.3vw] text-[1.5vh]'><p>Kickstart Your Coding Journey: <br />
         Collaborate on Real Projects and <br /> Grow with Expert Guidance at <br /> CodeX!</p></div>
@@ -21,13 +21,13 @@ function Home() {
       </div>
       <div className='w-full h-screen relative'>
         <div className='lg:text-[2vw] text-[2vh] absolute z-10 top-[14vh] left-[5vw]'> <h1 className='lg:w-[60vw] w-[70vw] capitalize font-extralight'><span className='lg:text-[2.5vw] text-[2.5vh] font-bold'>CodeX</span> Club at Woxsen University is a student-led organization aimed at building tech skills and fostering innovation. It offers a collaborative environment where students interested in coding, web development, and design come together to work on projects and develop new skills. Members participate in workshops, coding sessions, and hackathons, allowing them to gain hands-on experience in various tech fields.</h1></div>
-        <div className='lg:text-[2vw] text-[2vh] absolute z-10 bottom-[5vh] right-[5vw]'><h1 className='lg:w-[54vw] w-[65vw] text-end capitalize font-extralight'>The club emphasizes creativity, teamwork, and professional growth, helping members expand their technical knowledge and prepare for careers in the tech industry. With networking opportunities and exposure to real-world projects, CodeX is an ideal community for aspiring developers and tech enthusiasts at the university.</h1></div>
-        <div className=' absolute lg:right-0 lg:bottom-[-50vh] bottom-[-20vh]'><img src={circle} alt="" /></div>
+        <div className='lg:text-[2vw] text-[2vh] absolute z-10 bottom-[5vh] right-[5vw]'><h1 className='lg:w-[54vw] w-[70vw] text-end capitalize font-extralight'>The club emphasizes creativity, teamwork, and professional growth, helping members expand their technical knowledge and prepare for careers in the tech industry. With networking opportunities and exposure to real-world projects, CodeX is an ideal community for aspiring developers and tech enthusiasts at the university.</h1></div>
+        <div className=' absolute lg:right-0 lg:bottom-[-50vh] bottom-[-10vh]'><img src={circle} alt="" /></div>
       </div>
       <div className='w-full h-screen'>
-        <h1 className='text-[4vw] font-bold pt-[10vh] pl-[6vw]'>Meet Your Mentor!</h1>
-        <div className='flex mt-[4vh]'>
-            <div className='h-[79vh] w-1/2  pl-[5vw] flex items-center'>
+        <h1 className='lg:text-[4vw] text-[4vh] font-bold pt-[10vh] pl-[6vw]'>Meet Your Mentor!</h1>
+        <div className='flex flex-col-reverse lg:flex-row mt-[4vh]'>
+            <div className='h-[79vh] lg:w-1/2  pl-[5vw] flex items-center bg-gray-800'>
                 <div className='h-[70vh] w-[30vw] flex flex-col justify-between pl-[5vw] pb-[3vh]'>
                     <div className=''>
                         <h1 className='text-[4vw] w-1/2 leading-[4vw] font-light'>Amogh Deshmukh.</h1>
@@ -39,8 +39,8 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className=' h-[79vh] w-1/2  flex items-center justify-center'>
-                <div className='h-[79vh] w-[35vw] bg-zinc-800 mb-[13vh]'>
+            <div className=' h-[79vh] lg:w-1/2  flex items-center justify-center'>
+                <div className='h-[79vh] lg:w-[35vw] bg-zinc-800 mb-[13vh]'>
                     <img className='h-full w-full object-cover object-bottom' src={mentor} alt="" />
                 </div>
             </div>
