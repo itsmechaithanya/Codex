ba229d3211faff74ad3401d063e0533b540e9551
Chaithanya <itsmechaithanyam@gmail.com>
2025-01-24 11:26:06 +0530

    updated events.jsx

--- Codex-frontend/src/Components/Events.jsx
+++ Codex-frontend/src/Components/Events.jsx
@@ -25,11 +25,11 @@ function Events() {
           </div>
         </div>
       </div>
-      <div className='h-fit lg:h-screen flex flex-col lg:flex-row justify-center items-center'>
+      <div className='h-fit lg:h-screen flex flex-col lg:flex-row-reverse justify-between items-center lg:px-[12vw]'>
         <div className='lg:h-[80vh] lg:w-[33vw] w-[90vw] overflow-hidden'>
           <img className='w-full h-full' src={e1} alt="" />
         </div>
-        <div className='lg:h-[80vh] h-[13vh] lg:w-[33vw] w-[90vw] lg:ml-[10vw] flex flex-col justify-between lg:py-[5vh] lg:mt-[5vh] mt-[2vh] mb-[10vh] text-center lg:text-left'>
+        <div className='lg:h-[80vh] h-[13vh] lg:w-[33vw] w-[90vw]  flex flex-col justify-between lg:py-[5vh] lg:mt-[5vh] mt-[2vh] mb-[10vh] text-center lg:text-left'>
           <div>
             <h1 className='text-[3vh] lg:text-[3vw] capitalize font-medium'>Translation day contest</h1>
             <h1 className='text-[1.4vh] lg:text-[1.4vw] capitalize lg:w-2/3 lg:mt-[2vh] font-light'>E-certificates, cash Price and internship opportunities</h1>
@@ -39,20 +39,9 @@ function Events() {
           </div>
         </div>
       </div>
-      <div className='h-fit lg:h-screen w-full flex flex-col lg:flex-row justify-center items-center'>
-        <div className='lg:h-[80vh] lg:w-[33vw] w-[90vw] overflow-hidden'>
-          <img className='w-full h-full' src={e1} alt="" />
-        </div>
-        <div className='lg:h-[80vh] h-[13vh] lg:w-[33vw] w-[90vw] lg:ml-[10vw] flex flex-col justify-between lg:py-[5vh] lg:mt-[5vh] mt-[2vh] mb-[10vh] text-center lg:text-left'>
-          <div>
-            <h1 className='text-[3vh] lg:text-[3vw] capitalize font-medium'>Translation day contest</h1>
-            <h1 className='text-[1.4vh] lg:text-[1.4vw] capitalize lg:w-2/3 lg:mt-[2vh] font-light'>E-certificates, cash Price and internship opportunities</h1>
-          </div>
-          <div>
-            <h1 className='text-[1.3vh] lg:text-[1.3vw]'>On 3rd and 7th November 2024</h1>
-            
-          </div>
-        </div>
+      <div className='w-[76vw] h-[90vh] lg:mx-[12vw] bg-red-400'>
+        <div></div>
+        <div><img src="" alt="" /></div>
       </div>
       <Footer/>
     </div>
